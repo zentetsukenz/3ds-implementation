52c55e9373aeec6411c5a9c6d587bd14a9ab46e4
Chanapa <16201698+som-m@users.noreply.github.com>
2021-06-12 10:28:50 +0700

    Remove unused code (#20)

--- app/complete.php
+++ app/complete.php
@@ -1,8 +1,5 @@
 <?php
 
-$path = $_SERVER['PATH_INFO'];
-$order_id = filter_var($path, FILTER_SANITIZE_NUMBER_INT);
-
 $search = OmiseCharge::search($order_id);
 $charge_id = $search['data'][0]['id'];
 
